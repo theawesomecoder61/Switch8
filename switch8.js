7a7e71e266209430768375250e877d971b349671
Andrew <hellothere61@users.noreply.github.com>
2014-10-07 16:55:55 -0400

    Update switch8.js

--- switch8.js
+++ switch8.js
@@ -7,14 +7,7 @@ unminifed version of Switch8
 var Switch8 = function(options) {
   // variables
   var el = document.querySelector(options.elem);
-  
-  // loading switch8.css into the page
-  var head = document.head, link = document.createElement("link");
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = "https://raw.githack.com/theawesomecoder61/Switch8/master/switch8.css";
-  head.appendChild(link);
-  
+
   // adding .switch8, if .switch8 exists, say something
   if(!el.classList.contains("switch8")) {
     el.classList.add("switch8");
