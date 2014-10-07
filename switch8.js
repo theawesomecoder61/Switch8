3a55e17ba00ed4dafc57a66731dd6cf1e46165b8
Andrew <hellothere61@users.noreply.github.com>
2014-10-07 12:49:50 -0400

    alpha version

---
+++ switch8.js
@@ -0,0 +1,27 @@
+/*
+
+unminifed version of Switch8
+
+*/
+
+var Switch8 = function(options) {
+  // variables
+  var el = document.querySelector(options.elem);
+  
+  // adding .switch8, if .switch8 exists, say something
+  if(!el.classList.contains("switch8")) {
+    el.classList.add("switch8");
+  } else {
+    console.log("Switch already is Switch8'd! Ya dun goofed!");
+  }
+  
+  // checked
+  if(options.checked) {
+    el.setAttribute("checked", "checked");
+  }
+  
+  // disabled
+  if(options.disabled) {
+    el.setAttribute("disabled", "disabled");
+  }
+}
