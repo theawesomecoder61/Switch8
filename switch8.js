dd05a17260c36e2f48653ef94af01a191e5bd214
Andrew <hellothere61@users.noreply.github.com>
2014-10-08 12:36:28 -0400

    Create switch8.js

---
+++ switch8.js
@@ -0,0 +1,66 @@
+var Switch8 = function(options) {
+  // variables
+  var el = document.querySelector(options.elem);
+
+  // adding .switch8, if .switch8 exists, say something
+  if(!el.classList.contains("switch8")) {
+    el.classList.add("switch8");
+  } else {
+    console.log("Switch already is Switch8'd! Ya dun goofed!");
+  }
+
+  // checked
+  if(options.checked) {
+    el.setAttribute("checked", "checked");
+  }
+
+  // disabled
+  if(options.disabled) {
+    el.setAttribute("disabled", "disabled");
+  }
+
+  // colors
+    // checked
+    if(el.checked && options.checkedColor !== null) {
+      el.style.background = options.checkedColor;
+    } else {
+      if(!el.checked || options.checkedColor === null) {
+        el.style.background = "#e5e5e5";
+      }
+    }
+    // unchecked
+    if(!el.checked && options.uncheckedColor !== null) {
+      el.style.background = options.uncheckedColor;
+    } else {
+      if(el.checked || options.uncheckedColor === null) {
+        if(options.checkedColor !== null) {
+      	  el.style.background = options.checkedColor;
+        } else {
+      	  el.style.background = "#4cd964";
+        }
+      }
+    }
+
+  // toggle colors
+  el.onclick = function() {
+    // checked
+    if(el.checked && options.checkedColor !== null) {
+      el.style.background = options.checkedColor;
+    } else {
+      if(!el.checked || options.checkedColor === null) {
+        el.style.background = "#e5e5e5";
+      }
+    }
+    // unchecked
+    if(!el.checked && options.uncheckedColor !== null) {
+      el.style.background = options.uncheckedColor;
+    } else {
+      if(options.uncheckedColor === null) {
+        el.style.background = "#4cd964";
+      } else {
+        el.style.background = options.checkedColor;
+      }
+    }
+  }
+
+}
